b2ea97f3f2ba59f921296a684732ac6118f39837
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-30 13:51:39 +0100

    added views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.mixins import UpdateModelMixin, DestroyModelMixin
+
+from . import serializers
+from .models import Todo
+
 
 # Create your views here.
+class TodoAPIView(ListCreateAPIView, UpdateModelMixin, DestroyModelMixin):
+    queryset = Todo.objects.all()
+    serializer_class = serializers.TodoSerializer
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
