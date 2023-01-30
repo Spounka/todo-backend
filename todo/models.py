324d5616090d25ae3f5c8297383720c30d646108
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-30 13:04:30 +0100

    added todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(default="Title", max_length=30)
+    description = models.CharField(default="description", max_length=255)
+    done = models.BooleanField(default=False)
